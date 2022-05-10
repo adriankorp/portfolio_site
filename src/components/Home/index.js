bec9f3fb39301fcfeedc30e3b7d83994e09bcf57
adriankorp <adrian.korpowski@gmail.com>
2022-05-10 22:59:49 +0200

    Changing Skills and Home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,7 +5,6 @@ import { useEffect, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
 import Loader from 'react-loaders'
 
-
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
 
@@ -36,36 +35,37 @@ const Home = () => {
 
   return (
     <>
-        <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i,</span>
-          <br />
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m</span>
-          <img src={LogoTitle} alt="dev" />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={16}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            idx={21}
-          />
-        </h1>
-        <h2>Junior Python / Junior Java Script / Junior Blockchain</h2>
-        <Link to="/contact" className="flat-button">
-          CONTACT ME
-        </Link>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img src={LogoTitle} alt="dev" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={16}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={21}
+            />
+          </h1>
+          <h2>
+            Junior Python / Junior Java Script / Junior Blockchain Developer
+          </h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
       </div>
-    </div>
-    <Loader type="ball-clip-rotate-multiple"/>
+      <Loader type="ball-clip-rotate-multiple" />
     </>
-
   )
 }
 
--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,14 +1,55 @@
+import { useEffect, useState } from 'react'
 import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
 
-
 const Skills = () => {
-    return (
-        <>
+  const [letterClass, setLetterClass] = useState('text-animate')
 
-        <Loader type='ball-clip-rotate-multiple'/>
-        </>
-        )
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+  return (
+    <>
+      <div className="container skills-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['S', 'k', 'i', 'l', 'l', ' s', ' ', '&']}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['E', 'x', 'p', 'i', 'e', 'r', 'i', 'e', 'n', 'c', 'e']}
+              idx={23}
+            />
+          </h1>
+          <p>
+          I have been interested in the cryptocurrency market for over 2 years and that is why I started my adventure with programming.
+          </p>
+          <p>
+          At the very beginning, I learned to program in Python with the intention of writing a bot for playing the stock market. It wasn't easy, but I finished the bot script that is currently earning money. Unfortunately, I can't share the code on github.
+          </p>
+          <p>
+          I am currently learning JS syntax and looking for exploits in smart contract. I write, test and deploy, Smartcontracts in the brownie framework
+          </p>
+        </div>
+        <div className="skills-zone">
+            <div className='chart'>
+                <span>Python</span>
+                <footer>
+                    <div data-width="50%" style={{'width': '50%'}}></div>
+                </footer>
+            </div>
+        </div>
+      </div>
+      <Loader type="ball-clip-rotate-multiple" />
+    </>
+  )
 }
 
 export default Skills
