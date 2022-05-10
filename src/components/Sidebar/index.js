35332809ef4c00871cb46d5ebf084292c6a58b1b
adriankorp <adrian.korpowski@gmail.com>
2022-05-10 20:13:20 +0200

    New logos, delete Logo component folder

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
-import LogoS from '../../assets/images/logo-s.png'
+import LogoS from '../../assets/images/logo-a.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCode, faFileCode, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
