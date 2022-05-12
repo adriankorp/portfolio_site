9c63d4b6d403fa8934af76eb32d0ce27be9cf56b
adriankorp <adrian.korpowski@gmail.com>
2022-05-12 23:06:42 +0200

    Change smartcontract index.js and .scss

--- src/components/SmartContract/index.js
+++ src/components/SmartContract/index.js
@@ -5,6 +5,15 @@ import './index.scss'
 const SmartContract = () => {
     return (
         <>
+        <div className='container smartcontract-page'>
+            <div className='contract-data'>
+
+            </div>
+            <div className='swaps-data'>
+
+            </div>
+
+        </div>
 
         <Loader type='ball-clip-rotate-multiple'/>
         </>
